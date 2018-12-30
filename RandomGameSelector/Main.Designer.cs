31757391df1bc3e5da442483b3d2c2ca81ccd567
Unknown <kennethvanmele@hotmail.com>
2018-12-30 13:12:38 +0100

    Make font on labels and screen bigger

--- RandomGameSelector/Main.Designer.cs
+++ RandomGameSelector/Main.Designer.cs
@@ -46,14 +46,16 @@
             // lblPlatform
             // 
             this.lblPlatform.AutoSize = true;
-            this.lblPlatform.Location = new System.Drawing.Point(23, 28);
+            this.lblPlatform.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlatform.Location = new System.Drawing.Point(23, 37);
+            this.lblPlatform.MaximumSize = new System.Drawing.Size(230, 0);
             this.lblPlatform.Name = "lblPlatform";
-            this.lblPlatform.Size = new System.Drawing.Size(0, 17);
+            this.lblPlatform.Size = new System.Drawing.Size(0, 25);
             this.lblPlatform.TabIndex = 0;
             // 
             // btnGetGame
             // 
-            this.btnGetGame.Location = new System.Drawing.Point(12, 155);
+            this.btnGetGame.Location = new System.Drawing.Point(12, 206);
             this.btnGetGame.Name = "btnGetGame";
             this.btnGetGame.Size = new System.Drawing.Size(116, 27);
             this.btnGetGame.TabIndex = 1;
@@ -64,7 +66,7 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(134, 155);
+            this.btnCancel.Location = new System.Drawing.Point(227, 206);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(71, 27);
             this.btnCancel.TabIndex = 2;
@@ -75,9 +77,11 @@
             // lblGame
             // 
             this.lblGame.AutoSize = true;
-            this.lblGame.Location = new System.Drawing.Point(26, 72);
+            this.lblGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGame.Location = new System.Drawing.Point(23, 93);
+            this.lblGame.MaximumSize = new System.Drawing.Size(230, 0);
             this.lblGame.Name = "lblGame";
-            this.lblGame.Size = new System.Drawing.Size(0, 17);
+            this.lblGame.Size = new System.Drawing.Size(0, 25);
             this.lblGame.TabIndex = 3;
             // 
             // menuStrip1
@@ -87,7 +91,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(220, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(310, 28);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -144,7 +148,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(220, 194);
+            this.ClientSize = new System.Drawing.Size(310, 245);
             this.Controls.Add(this.lblGame);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnGetGame);
